ac0b3faaea989332a82309751071ab09ab9e9d8e
Catalin Esanu <cesanu@microsoft.com>
2017-05-23 11:42:17 +0300

    changed HDI to 3.6 with Spark 2.1 + Made DCOS optional

--- runtime/schedule_job.sh
+++
@@ -1,6 +0,0 @@
-export PREFIX=`hostname | cut -c 1-3`
-if [ "$PREFIX" = "hn0" ]; then
-	sudo crontab -l > cronfile
-	echo 30 \* \* \* \* /usr/hdp/current/spark2-client/bin/spark-submit --master yarn --deploy-mode cluster --name PARSE_AVRO --num-executors 2 --executor-cores 2 --conf spark.executor.memory=2G  --conf spark.yarn.executor.memoryOverhead=1024 --conf spark.yarn.driver.memoryOverhead=4072 --conf spark.driver.memory=1G --conf spark.driver.cores=2  --conf spark.jars.packages='com.databricks:spark-avro_2.11:3.1.0' wasbs://hdiscripts@hdiacc75430.blob.core.windows.net/process_avro.py >> cronfile
-	sudo crontab cronfile
-fi	
